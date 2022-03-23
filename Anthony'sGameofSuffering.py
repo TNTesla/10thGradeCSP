b76c09bc5696a6f0f72dd8d41ca6058911c91e44
TNTesla <57197318+TNTesla@users.noreply.github.com>
2022-03-23 10:07:41 -0400

    Update and rename Anthony's Game of Suffering to Anthony'sGameofSuffering.py

---
+++ Anthony'sGameofSuffering.py
@@ -0,0 +1,30 @@
+
+from tkinter import *
+import random
+from tkinter import messagebox
+
+#create a root window
+
+root = Tk()
+
+
+"""""""""""""""""""""""""""""""""""""""""""""""""""
+"""           """"""           """"""           """
+"""           """"""           """"""           """
+"""""""""""""""""""""""""""""""""""""""""""""""""""
+
+
+#config window size and color and title and whatever
+root.title("Conway's Game of Life (reconstructed by Mark Anthony)")
+root.geometry("1600x1000")
+root.config(bg="#232323")
+
+#defining functions
+
+
+
+#
+
+
+
+root.mainloop()
