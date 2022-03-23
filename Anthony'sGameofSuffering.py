1be91f0f40f2945e631a3e29eb45018e830c8cd9
TNTesla <57197318+TNTesla@users.noreply.github.com>
2022-03-23 10:39:58 -0400

    Update Anthony'sGameofSuffering.py

--- Anthony'sGameofSuffering.py
+++ Anthony'sGameofSuffering.py
@@ -1,4 +1,3 @@
-
 from tkinter import *
 import random
 from tkinter import messagebox
@@ -6,25 +5,27 @@ from tkinter import messagebox
 #create a root window
 
 root = Tk()
-
+canvas = Canvas(root, width=500, height=500, bg = "#4F4F4F")
 
 """""""""""""""""""""""""""""""""""""""""""""""""""
 """           """"""           """"""           """
 """           """"""           """"""           """
 """""""""""""""""""""""""""""""""""""""""""""""""""
-
 
 #config window size and color and title and whatever
 root.title("Conway's Game of Life (reconstructed by Mark Anthony)")
-root.geometry("1600x1000")
-root.config(bg="#232323")
+root.geometry("500x500")
+root.config(bg="white")
 
 #defining functions
 
 
 
-#
+#making a lot of lines and shit
+for i in range(55):
+    canvas.create_line(-10, (i*10), 510, (i*10), width = 1)
+    canvas.create_line((i*10), -10, (i*10), 500, width = 1)
 
-
+canvas.pack()
 
 root.mainloop()
