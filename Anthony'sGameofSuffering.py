c1f27fda92f28a5b47911b786d9a4bfb00e2141c
TNTesla <57197318+TNTesla@users.noreply.github.com>
2022-03-25 08:51:52 -0400

    Update Anthony'sGameofSuffering.py

--- Anthony'sGameofSuffering.py
+++ Anthony'sGameofSuffering.py
@@ -1,31 +1,52 @@
 from tkinter import *
 import random
 from tkinter import messagebox
+import copy
 
 #create a root window
 
 root = Tk()
-canvas = Canvas(root, width=500, height=500, bg = "#4F4F4F")
+canvas = Canvas(root, width=506, height=506, bg = "#4F4F4F")
 
-"""""""""""""""""""""""""""""""""""""""""""""""""""
-"""           """"""           """"""           """
-"""           """"""           """"""           """
-"""""""""""""""""""""""""""""""""""""""""""""""""""
+""""""""""""""""""""""""""""""""""""""""""""""""""
 
 #config window size and color and title and whatever
 root.title("Conway's Game of Life (reconstructed by Mark Anthony)")
-root.geometry("500x500")
+root.geometry("510x510")
 root.config(bg="white")
+
+#defining variables
+
+
 
 #defining functions
 
+def Cell(x, y):
+    canvas.create_line(x, y, x+8, y, width = 5, fill='yellow')
+    
 
+def togCel(x, y):
+    pass
 
 #making a lot of lines and shit
-for i in range(55):
-    canvas.create_line(-10, (i*10), 510, (i*10), width = 1)
-    canvas.create_line((i*10), -10, (i*10), 500, width = 1)
+
+for i in range(51):
+    canvas.create_line(4, 4+(i*10), 505, 4+(i*10), width = 1)
+    canvas.create_line(4+(i*10), 4, 4+(i*10), 505, width = 1)
+    
+canvas.create_line(4, 4, 4, 505)
+canvas.create_line(4, 4, 505, 4)
 
 canvas.pack()
 
+grid = []
+row = []
+for i in range(25):
+    row.append(0)
+for i in range(25):
+    grid.append(copy.deepcopy(row))
+
+add_mouse_click_handler(togCel)
+
+#keeping the interface up
 root.mainloop()
